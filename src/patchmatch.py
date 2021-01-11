dbcffc4b9461466adb52079fcf98401f1d51a302
Alexandre Perez <33580936+alexprz@users.noreply.github.com>
2021-01-11 13:54:24 +0100

    Factorize init

--- src/patchmatch.py
+++ src/patchmatch.py
@@ -26,8 +26,42 @@ class PatchMatchInpainting():
         self.alpha = alpha
         self.beta = img.size[0] if beta is None else beta
 
-    # def _init_offsets(self, img_shape):
+    @staticmethod
+    def _coords_from_bbox(bbox):
+        x1, y1, x2, y2 = bbox
+        return x1, y1, x2, y2
 
+    @staticmethod
+    def _get_bbox_wh(bbox):
+        x1, y1, x2, y2 = self._coords_from_bbox(bbox)
+        return x2-x1, y2-y1
+
+    @staticmethod
+    def _get_mask(img_shape, bbox):
+        x1, y1, x2, y2 = self._coords_from_bbox(bbox)
+        mask = np.zeros(img_shape)
+        mask[x1:x2, y1:y2] = 1
+        return mask
+
+    @staticmethod
+    def _init_offsets(img_shape, bbox):
+        w, h = self._get_bbox_wh(bbox)
+        mask = self._get_mask(img_shape, bbox)
+
+        # Retrieve all the coordinates outside the mask (the B image)
+        b_coords = np.where(mask == 0)
+
+        # Randomly sample from these coordinates
+        match_idx = np.random.choice(np.arange(b_coords[0].shape[0]), size=w*h)
+        x_pos = b_coords[0][match_idx].reshape(w, h)
+        y_pos = b_coords[1][match_idx].reshape(w, h)
+        positions_in_b = np.stack((x_pos, y_pos), axis=2)
+
+        # Coordinates in the mask (image A)
+        positions_in_a = np.stack(np.meshgrid(np.arange(w), np.arange(h)), axis=2)
+
+        # Return offsets
+        return positions_in_b - positions_in_a
 
     def inpaint_from_bbox(self, bbox):
         """Inpaint a mask region given by a bbox.
@@ -48,20 +82,11 @@ class PatchMatchInpainting():
         x_a, y_a = bbox[0], bbox[1]
         w_a, h_a = img_a.size
 
-        mask = np.zeros(self.original_img.size)
-        mask[x_a:x_a+w_a, y_a:y_a+h_a] = 1
+        f = self._init_offsets(self.original_img.size, bbox)
 
-        b_coords = np.where(mask == 0)
+        print(f)
+        print(f.shape)
 
-        match_idx = np.random.choice(np.arange(b_coords[0].shape[0]), size=w_a*h_a)
-        x_pos = b_coords[0][match_idx].reshape(w_a, h_a)
-        y_pos = b_coords[1][match_idx].reshape(w_a, h_a)
-
-        positions_in_b = np.stack((x_pos, y_pos), axis=2)
-        positions_in_a = np.stack(np.meshgrid(np.arange(w_a), np.arange(h_a)), axis=2)
-
-        # Compute offsets
-        f = positions_in_b - positions_in_a
 
 
 
