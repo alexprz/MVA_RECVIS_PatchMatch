5b0797bbf7f5443a07df901d8058b9d18c198487
Alexandre Perez <33580936+alexprz@users.noreply.github.com>
2021-01-24 13:04:02 +0100

    Retrieve files

--- src/evaluate.py
+++ src/evaluate.py
@@ -13,10 +13,37 @@ class Examiner():
         self.root = root
         self.ext = ext
 
+        # Required filenames
+        self.img_filename = f'img.{ext}'
+        self.mask_filename = f'mask.{ext}'
+        self.inpainted_filename = f'inpainted.{ext}'
+
+        self.required_filenames = set([
+            self.img_filename,
+            self.mask_filename,
+            self.inpainted_filename
+        ])
+
+        self.img_folder_paths = self._retrieve_paths()
+
     def _retrieve_paths(self):
         walk = os.walk(self.root)
 
-        print(walk)
+        img_folder_paths = []
+
+        for folder, subfolders, filenames in walk:
+            # Keep only leaf folders
+            if subfolders:
+                continue
+
+            # Check if required files are present
+            if not self.required_filenames.issubset(filenames):
+                print(f'Warning: dir {folder} ignored because doesnt contain '
+                      f'the required filenames {self.required_filenames}.')
+
+            img_folder_paths.append(folder)
+
+        return img_folder_paths
 
     def evaluate(self):
         pass
